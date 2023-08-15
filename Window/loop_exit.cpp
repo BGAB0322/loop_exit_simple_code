ba723e520f942110b3e5bd1550fa7e6bdac40279
BGAB <79795395+BGAB0322@users.noreply.github.com>
2023-08-15 19:23:06 +0900

    Create loop_exit.cpp
    loop_exit example in linux

---
+++ Window/loop_exit.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <signal.h> //signal header
+
+bool signal_handle = false;
+
+void interruptSignal(int signal)
+{
+    (void)signal;
+    signal_handle = true;  
+}
+
+int main()
+{
+    signal(SIGINT, interruptSignal);
+
+    while (true && !signal_handle)
+    {
+        printf("Hello World!\n");
+    }
+}
